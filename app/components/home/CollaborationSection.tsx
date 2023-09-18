e51833e327a55c16738be5bbc0b1b8d4bc1f916d
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-18 21:05:43 +0100

    refactor collaboration section

---
+++ app/components/home/CollaborationSection.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import { cormorant_garamond, montserrat } from "@/app/utils/font";
+import Button from "../button";
+
+interface ListItemProps {
+  text: string;
+}
+
+const ListItem = ({ text }: ListItemProps): JSX.Element => (
+  <li
+    className={`${cormorant_garamond.className} font-medium text-xl max-lg:text-center mb-1`}
+  >
+    {text}
+  </li>
+);
+
+export default function SectionTwo(): JSX.Element {
+  return (
+    <section className="px-5 sm:px-10 lg:px-20 py-16 lg:py-20">
+      <div className="grid lg:grid-cols-2 gap-10">
+        <div className="flex justify-center items-center">
+          <Image
+            src="/patience-brown-2.jpg"
+            alt="picture of patience brown"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="flex flex-col justify-center">
+          <header className="max-lg:text-center mb-10">
+            <h4
+              className={`${cormorant_garamond.className} font-medium text-5xl`}
+            >
+              Let me ensure you share that message
+            </h4>
+          </header>
+          <ul className="list-disc list-inside mb-12">
+            <span
+              className={`block ${montserrat.className} text-yinmn-blue font-semibold text-lg max-lg:text-center uppercase mb-2`}
+            >
+              together, we will:
+            </span>
+            <ListItem text="Define your audience and clarify your message" />
+            <ListItem text="Organize your thoughts into a detailed outline" />
+            <ListItem text="Communicate your message so compellingly that it'll bear the fruits you envisioned" />
+          </ul>
+          <div className="flex max-lg:justify-center">
+            <Button url="/about" title="meet patience brown" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ app/components/home/ProcessSection.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { cormorant_garamond, montserrat } from "@/app/utils/font";
+import { processes } from "@/app/data/Processes";
+import Button from "../button";
+
+export default function ProcessSection(): JSX.Element {
+  return (
+    <section className="bg-seasalt px-5 sm:px-10 lg:px-20 py-16 lg:py-20">
+      <div className="flex flex-col items-center">
+        <header className="flex flex-col items-center gap-4 text-center mb-12">
+          <p
+            className={`${montserrat.className} text-yinmn-blue font-semibold text-lg uppercase`}
+          >
+            the 3-step process to:
+          </p>
+          <h4
+            className={`${cormorant_garamond.className} text-black font-medium text-5xl`}
+          >
+            Get Your Book, Your Voice, Your message, Out There
+          </h4>
+        </header>
+        <ul className="grid lg:grid-cols-3 gap-5 mb-12">
+          {processes.map(({ icon, heading, content }, index: number) => (
+            <li key={index} className="flex flex-col items-center text-center">
+              <div className="mb-6">
+                <Image src={icon} alt="step icon" width={80} height={80} />
+              </div>
+              <div
+                className={`${montserrat.className} text-yinmn-blue font-semibold text-lg uppercase mb-3`}
+              >
+                {heading}
+              </div>
+              <div className="flex flex-col items-center gap-2">{content}</div>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <Button url="/process" title="learn more" />
+        </div>
+      </div>
+    </section>
+  );
+}
