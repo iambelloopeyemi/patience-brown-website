01c5364f97f4ae7003102f63934015c13f1a9407
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-18 23:02:23 +0100

    refactor benefit section

---
+++ app/components/BeneficiariesComponent.tsx
@@ -0,0 +1,30 @@
+// BeneficiariesComponent.tsx
+import Image from "next/image";
+import { montserrat } from "../utils/font";
+import { BeneficiaryProps } from "../data/Beneficiaries";
+
+interface BeneficiariesComponentProps {
+  content: BeneficiaryProps[];
+}
+
+export default function BeneficiariesComponent({
+  content,
+}: BeneficiariesComponentProps): JSX.Element {
+  return (
+    <>
+      {content.map(({ src, alt, title, text }, index: number) => (
+        <li key={index} className="flex flex-col items-center">
+          <div className="mb-3">
+            <Image src={src} alt={alt} />
+          </div>
+          <div
+            className={`${montserrat.className} text-yinmn-blue font-semibold text-lg uppercase mb-2`}
+          >
+            {title}
+          </div>
+          <div className="text-lg">{text}</div>
+        </li>
+      ))}
+    </>
+  );
+}
---
+++ app/components/home/BenefitSection.tsx
@@ -0,0 +1,36 @@
+import { cormorant_garamond } from "@/app/utils/font";
+import BeneficiariesComponent from "../BeneficiariesComponent";
+import { BeneficiaryContent } from "@/app/data/Beneficiaries";
+
+export default function BenefitSection(): JSX.Element {
+  return (
+    <section className="bg-seasalt px-5 sm:px-10 lg:px-20 py-16 lg:py-20">
+      <div className={`${cormorant_garamond.className}`}>
+        <article className="flex flex-col items-center font-medium text-center">
+          <h3 className="text-black text-5xl mb-3">
+            Get your message to the people in need of it
+          </h3>
+          <p className="text-2xl mb-2">
+            Many visionaries have a message for their world, books they&apos;ve
+            been planning to write, but they got stuck, couldn&apos;t find the
+            time to continue, and began to wonder if they&apos;d ever share that
+            message
+          </p>
+          <p className="text-2xl">
+            But with a ghostwriter to help write your book, your vision stays
+            alive.
+          </p>
+        </article>
+        <hr className="my-12" />
+        <article className="flex flex-col items-center font-medium text-center">
+          <div className="text-4xl mb-10">
+            Who can benefit from a ghostwriter and book editor?
+          </div>
+          <ul className="grid lg:grid-cols-3 gap-3">
+            <BeneficiariesComponent content={BeneficiaryContent} />
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+}
