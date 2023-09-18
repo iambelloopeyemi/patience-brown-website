7b66328b2d2853ffb103443e9ffbe7c39e38f485
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-18 15:34:59 +0100

    remove margin top from footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   const currentYear: number = new Date().getFullYear();
   return (
-    <footer className="bg-yinmn-blue py-[60px] text-seasalt mt-10">
+    <footer className="bg-yinmn-blue py-[60px] text-seasalt">
       <div className="flex flex-col items-center gap-8">
         <div>
           <Image
