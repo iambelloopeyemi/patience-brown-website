ff5ba0cbe100f5425a1cb26a30b0c33deb891045
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-28 15:23:39 +0100

    add hero component to schedule a call page

--- app/schedule-a-call/page.tsx
+++ app/schedule-a-call/page.tsx
@@ -1,3 +1,12 @@
+import HeroComponent from "../components/HeroComponent";
+
 export default function scheduleACallPage() {
-  return <div>scheduleACallPage</div>;
+  return (
+    <>
+      <HeroComponent
+        heading={`Finally Finish Your Book`}
+        message={`Fill out the form below to start the conversation. You can expect a response within 1-2 business days.`}
+      />
+    </>
+  );
 }
