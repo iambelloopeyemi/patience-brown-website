364a102982dfe4523ea4575165625f829c4663e7
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-28 21:23:00 +0100

    refactor footer component

--- app/components/FooterComponent.tsx
+++ app/components/FooterComponent.tsx
@@ -1,37 +1,42 @@
 import Link from "next/link";
-import Nav from "./NavComponent";
+import { montserrat } from "../utils/font";
 import Logo from "./LogoComponent";
+import Nav from "./NavComponent";
 
 export default function Footer(): JSX.Element {
   const currentYear: number = new Date().getFullYear();
 
   return (
-    <footer className="bg-yinmn-blue px-4 py-16 text-seasalt">
+    <footer
+      className={`bg-yinmn-blue px-5 py-16 ${montserrat.className} text-seasalt`}
+    >
       <div className="flex flex-col items-center gap-8 text-center">
-        <Logo />
+        <div>
+          <Logo />
+        </div>
         <ul className="flex max-sm:flex-col items-center gap-4">
           <Nav
-            activeAnchorTagClass="font-semibold tracking-widest uppercase hover:underline"
-            anchorTagClass="font-semibold tracking-widest uppercase hover:underline"
+            activeAnchorTagClass="font-semibold uppercase text-[16px] leading-[14px] tracking-widest hover:underline"
+            anchorTagClass="font-semibold uppercase text-[16px] leading-[14px] tracking-widest hover:underline"
           />
         </ul>
         <div>
           <Link
             href={`#`}
-            className="font-medium tracking-wide hover:underline"
+            className="font-medium text-[14px] leading-[23px] tracking-wide hover:underline"
           >
             Privacy Policy
           </Link>
           &nbsp;•&nbsp;
           <Link
             href={`#`}
-            className="font-medium tracking-wide hover:underline"
+            className="font-medium text-[14px] leading-[23px] tracking-wide hover:underline"
           >
             Terms
           </Link>
         </div>
         <p>
-          <strong className="font-light tracking-tight">
+          <strong className="font-light text-[14px] leading-[23px] tracking-tight">
             Patience Brown © {currentYear}
           </strong>
         </p>
